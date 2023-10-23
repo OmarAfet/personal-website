e1cfe2dd765ca3e0caf909bfbf9a6e9081febef4
OmarAfet <OmarAlenezy2004@gmail.com>
2023-10-24 01:55:33 +0300

    gap-32 to gap-16

--- components/Pages/Hero.tsx
+++ components/Pages/Hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
 	const myAge = new Date().getFullYear() - 2004;
 
 	return (
-		<div className="flex md:flex-row flex-col-reverse lg:gap-48 gap-32 justify-center items-center">
+		<div className="flex md:flex-row flex-col-reverse lg:gap-48 gap-16 justify-center items-center">
 			<div className="flex flex-col gap-4">
 				<div className="header">Hello, I&apos;m Omar</div>
 				<div className="subtitle">Hello There! I&apos;m Omar Afet, a {myAge}-year-old freelance developer with an insatiable passion for programming. My ethos revolves around three pillars: Honesty, Simplicity, and Logic. These principles not only shape my coding endeavors but also resonate in every decision I make.</div>
