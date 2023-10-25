7139f530a5f49f33c1d330dab5be65ee48e4b892
OmarAfet <OmarAlenezy2004@gmail.com>
2023-10-26 02:46:57 +0300

    remove rel

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -6,13 +6,13 @@ export default function Socials({ size }: { size: number }) {
 
 	return (
 		<div className="flex gap-2 text-left items-center">
-			<Link className="opacity-50 hover:opacity-100" target="_blank" href="https://x.com/OmarAfet" rel="noopener noreferrer">
+			<Link className="opacity-50 hover:opacity-100" target="_blank" href="https://x.com/OmarAfet">
 				<RiTwitterXFill style={{ height: customSize, width: customSize }} />
 			</Link>
-			<Link className="opacity-50 hover:opacity-100" target="_blank" href="https://github.com/OmarAfet" rel="noopener noreferrer">
+			<Link className="opacity-50 hover:opacity-100" target="_blank" href="https://github.com/OmarAfet">
 				<RiGithubFill style={{ height: customSize, width: customSize }} />
 			</Link>
-			<Link className="opacity-50 hover:opacity-100" target="_blank" href="https://www.linkedin.com/in/omarafet/" rel="noopener noreferrer">
+			<Link className="opacity-50 hover:opacity-100" target="_blank" href="https://www.linkedin.com/in/omarafet/">
 				<RiLinkedinBoxFill style={{ height: customSize, width: customSize }} />
 			</Link>
 		</div>
