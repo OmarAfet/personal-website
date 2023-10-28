c0eefcf19ec737cc46aa7c6ecd376e6ebb35d3b4
Omar Afet <135471287+OmarAfet@users.noreply.github.com>
2023-10-28 05:28:53 +0300

    replace my age with `teenager`

--- components/Pages/Hero.tsx
+++ components/Pages/Hero.tsx
@@ -2,13 +2,11 @@ import Socials from "@components/Socials";
 import Image from "next/image";
 
 export default function Hero() {
-	const myAge = new Date().getFullYear() - 2004;
-
 	return (
 		<div className="flex md:flex-row flex-col-reverse lg:gap-48 gap-16 justify-center items-center">
 			<div className="flex flex-col gap-4">
 				<div className="header">Hello, I&apos;m Omar</div>
-				<div className="subtitle">Hello There! I&apos;m Omar Afet, a {myAge}-year-old freelance developer with an insatiable passion for programming. My ethos revolves around three pillars: Honesty, Simplicity, and Logic. These principles not only shape my coding endeavors but also resonate in every decision I make.</div>
+				<div className="subtitle">Hello There! I&apos;m Omar Afet, a teenager freelance developer with an insatiable passion for programming. My ethos revolves around three pillars: Honesty, Simplicity, and Logic. These principles not only shape my coding endeavors but also resonate in every decision I make.</div>
 				<Socials size={32} />
 			</div>
 			<Image priority className="w-64 h-64 aspect-square rounded-full" src="/Avatar.png" alt="Avatar" width={1080} height={1080} />
