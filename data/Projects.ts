060318c3db4ffd71109a6d3e4d3a7d322cb5861d
Omar Afet <135471287+OmarAfet@users.noreply.github.com>
2023-10-28 07:01:06 +0300

    Update Projects.ts

--- data/Projects.ts
+++ data/Projects.ts
@@ -7,7 +7,7 @@ interface Projects {
 const projects: Projects[] = [
 	{ title: "Image Average Color", description: "Image Average Color is a tool that calculates the average color of uploaded images and displays it in RGB, HEX, and HSV.", link: "https://omarafet.github.io/Image-Average-Color/" },
 	{ title: "CoderHub", description: "Solutions to various coding challenges from CoderHub.", link: "https://github.com/OmarAfet/CoderHub" },
-	{ title: "SmartAI", description: "SmartAI is a place where creativity thrives. We are a team of creators and dedicated researchers aiming to use artificial intelligence to enhance people&apos;s lives.", link: "https://omarafet.github.io/SmartAI/" },
+	{ title: "SmartAI", description: "SmartAI is a place where creativity thrives. We are a team of creators and dedicated researchers aiming to use artificial intelligence to enhance people's lives.", link: "https://omarafet.github.io/SmartAI/" },
 ];
 
 export default projects;
